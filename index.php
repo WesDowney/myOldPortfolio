e9aa0d7230f1d440c93aa5fc9376003cd9cd391e
Wes <wesdowney@gmail.com>
2017-01-10 11:47:14 -0500

    Added Google Analytics

--- index.php
+++ index.php
@@ -288,6 +288,17 @@ if (!empty($_POST)) {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90145405-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <script type="text/javascript">
 
 function validateEmail($email) {
